a4512d2528ba2fba5ef7021aac61c68fba58b6da
Collins Koech <collinskoechck34@gmail.com>
2020-11-19 21:43:46 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'sideNav';
 import './App.css';
+import Navbar from './Navigators/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        
+      <Navbar/>
       </div>
     </Router>
   );
